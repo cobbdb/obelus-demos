7c3e4ab1e8c0adf108a6214c3ee7760190901ef3
cobbdb <cobbdb@gmail.com>
2015-01-29 12:45:51 -0800

    update for 1.0.x

--- static/loader.js
+++ static/loader.js
@@ -1,7 +1,7 @@
 var obelus = obelus || {};
 obelus.cmd = obelus.cmd || [];
 obelus.cmd.push(function () {
-    obelus.enable({
+    obelus.launcher.init('../static/om-sdk.html', {
         PUBLISHER_ID: 'b503cf67-5b4a-49fc-91b7-0772b6d63994',
         ADSERVER_AUTHORIZATION: 'ApiKey dcobb:af8f6211357cbfaa3a266d314a9ec09bdb93cff1',
         SHOW_ADS_ON_DESKTOP_DEVICES: true
