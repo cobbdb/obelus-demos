2afa1152f7877e33afe26f68f5bc6341721c1aec
cobbdb <cobbdb@gmail.com>
2015-01-29 08:26:25 -0800

    update auth

--- kirotv/loader.js
+++ kirotv/loader.js
@@ -3,7 +3,7 @@ obelus.cmd = obelus.cmd || [];
 obelus.cmd.push(function () {
     obelus.enable({
         PUBLISHER_ID: 'b503cf67-5b4a-49fc-91b7-0772b6d63994',
-        ADSERVER_AUTHORIZATION: 'ApiKey api:5e04178b8c685f6bd2d36f0cb8d337af8acaffc3',
+        ADSERVER_AUTHORIZATION: 'ApiKey dcobb:af8f6211357cbfaa3a266d314a9ec09bdb93cff1',
         SHOW_ADS_ON_DESKTOP_DEVICES: true,
         DEBUG: true
     });
