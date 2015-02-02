b1b32ed27f181c3e0f7c63e59548b689f9ad2c86
cobbdb <cobbdb@gmail.com>
2015-02-01 20:00:25 -0800

    shared loader and new integ creds

--- static/loader.js
+++ static/loader.js
@@ -2,8 +2,8 @@ var obelus = obelus || {};
 obelus.cmd = obelus.cmd || [];
 obelus.cmd.push(function () {
     obelus.enable({
-        PUBLISHER_ID: 'b503cf67-5b4a-49fc-91b7-0772b6d63994',
-        ADSERVER_AUTHORIZATION: 'ApiKey dcobb:af8f6211357cbfaa3a266d314a9ec09bdb93cff1',
+        PUBLISHER_ID: 'ef501c36-d2ab-4e6e-8d1f-013bcd395842',
+        ADSERVER_AUTHORIZATION: 'ApiKey dcobb:cb720d82363c04d9914b2491824eb135e52350cd',
         SHOW_ADS_ON_DESKTOP_DEVICES: true
     });
 });
