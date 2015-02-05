7b703a891ece5a501da27f26e3da99fc1665aaf4
cobbdb <cobbdb@gmail.com>
2015-02-05 14:49:38 -0800

    use integ

--- static/loader.js
+++ static/loader.js
@@ -2,14 +2,17 @@ var obelus = obelus || {};
 obelus.cmd = obelus.cmd || [];
 obelus.cmd.push(function () {
     /**
-     * Test Publishers:
+     * ### Dev: api:5e04178b8c685f6bd2d36f0cb8d337af8acaffc3
      * Mobile: bc4208b3-d01b-4f5f-935a-58b7923d6d01
      * Desktop: fd571a6e-af4c-4ea4-acc9-22cdad9f1e5e
      * Both: 9e56a2c6-fc72-4c7d-8175-1a5c47d3abf0
+     *
+     * ### Integ: dcobb:cb720d82363c04d9914b2491824eb135e52350cd
+     * Integ: ef501c36-d2ab-4e6e-8d1f-013bcd395842
      */
     obelus.enable({
-        publisherId: '9e56a2c6-fc72-4c7d-8175-1a5c47d3abf0',
-        authId: 'ApiKey api:5e04178b8c685f6bd2d36f0cb8d337af8acaffc3',
+        publisherId: 'ef501c36-d2ab-4e6e-8d1f-013bcd395842',
+        authId: 'ApiKey dcobb:cb720d82363c04d9914b2491824eb135e52350cd',
         desktop: true,
         minLife: 6000,
         frequency: 1,
