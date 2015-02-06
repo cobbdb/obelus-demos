fd5c3f82b933c52ca1385bdec2b08ffe586685e7
cobbdb <cobbdb@gmail.com>
2015-02-05 22:44:51 -0800

    .

--- tasks/template.js
+++ tasks/template.js
@@ -3,13 +3,13 @@ var str = require('curb'),
     version = 'v2';
 
 /**
- * ### Dev: api:5e04178b8c685f6bd2d36f0cb8d337af8acaffc3
- * Mobile: bc4208b3-d01b-4f5f-935a-58b7923d6d01
- * Desktop: fd571a6e-af4c-4ea4-acc9-22cdad9f1e5e
- * Both: 9e56a2c6-fc72-4c7d-8175-1a5c47d3abf0
+ * ### Dev:
+ * api:5e04178b8c685f6bd2d36f0cb8d337af8acaffc3
+ * 9e56a2c6-fc72-4c7d-8175-1a5c47d3abf0
  *
- * ### Integ: dcobb:cb720d82363c04d9914b2491824eb135e52350cd
- * Integ: 5efa5780-31f5-4a89-8d98-4a242edad533
+ * ### Integ:
+ * dcobb:cb720d82363c04d9914b2491824eb135e52350cd
+ * 5efa5780-31f5-4a89-8d98-4a242edad533
  */
 module.exports = function (grunt) {
     grunt.config.merge({
